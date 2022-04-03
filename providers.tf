603ec4d08304bfbb5994eae9aac773ae13375c8c
tisha <vumudnet@gmail.com>
2022-04-03 23:26:56 +0200

    edit providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
